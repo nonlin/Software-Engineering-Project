84cd3ff93a63acfc2366f8946dde5bf7247aea7e
nonlin <gerfeso@live.com>
2014-09-29 22:16:02 -0500

    GUI

---
+++ TTT/Assets/Scripts/GameBoardScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameBoardScene : MonoBehaviour {
+
+	public void ChangeToScene(int sceneToChangeTo){
+		Application.LoadLevel (sceneToChangeTo);
+	}
+}
---
+++ TTT/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+	
+	private Animator menuAnim;
+	private bool showDifficulty = false; 
+
+	// Use this for initialization
+	void Start () {
+		
+		menuAnim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void showAI(){
+
+			menuAnim.SetBool("show",true);
+	}
+
+	public void hideAI(){
+		menuAnim.SetBool("show",false);
+	}
+
+}
