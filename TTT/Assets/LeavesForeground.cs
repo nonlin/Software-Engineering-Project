ba11276e5a8fc452f271fd98880ab43527cd69c0
nonlin <gerfeso@live.com>
2014-11-26 01:46:27 -0600

    Major GUI Changes
    Changed MainMenu Scene and GameBoard Scene look.

---
+++ TTT/Assets/LeavesForeground.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeavesForeground : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		particleSystem.renderer.sortingLayerName = "Foreground";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
